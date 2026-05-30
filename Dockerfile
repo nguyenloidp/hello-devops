94c51f33e22ba2534d396f68afde7ff9b61bf8b8
nguyenloidp <nguyenloidp@gmail.com>
2026-05-31 00:05:35 +0700

    full ci cd version tracking

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ FROM python:3.12-slim
 WORKDIR /app
 
 COPY requirements.txt .
-
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 5000
+ENV APP_VERSION=local
 
-CMD ["gunicorn","-b","0.0.0.0:5000","app:app"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2"
+    version = os.getenv("APP_VERSION", "local")
+    return f"🚀 Hello DevOps - Version: {version}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
